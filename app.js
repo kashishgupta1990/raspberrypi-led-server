5852a6496b1540362eac681dc983054b95c18765
Shubham Gupta <charmshubham@gmail.com>
2016-03-27 15:25:53 +0530

    update the code

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
 var http = require('http');
 var gpio = require('pi-gpio');
 
-var GPIO = {
-    init:function(){
-        gpio.open(16, "output", function(err) {		// Open pin 16 for output
-            if(err){
-                throw err;
+// GPIO basic setup
+var GPIO_PIN = {
+  SIXTEEN:16  
+};
+gpio.open(GPIO_PIN.SIXTEEN, "output", function(err) {		// Open pin 16 for output
+    if(err){
+        throw err;
+    }
+
+    startHttpServer();
+});
+
+var LED = {
+    on:function(){
+        gpio.read(GPIO_PIN.SIXTEEN, function(err, value) {
+            if(err) throw err;
+            if(value == 0){
+                gpio.write(GPIO_PIN.SIXTEEN, 1, function() {});
             }
         });
     },
-    on:function(){
-        GPIO.led(1)
-    },
     off:function(){
-        GPIO.led(0)
-    },
-    led:function(mode){
-        gpio.write(16, mode, function() {			// Set pin 16 high (1)
-            gpio.close(16);						// Close pin 16
+        gpio.read(GPIO_PIN.SIXTEEN, function(err, value) {
+            if(err) throw err;
+            if(value == 1){
+                gpio.close(GPIO_PIN.SIXTEEN);
+            }
         });
     }
 };
 
-http
-    .createServer(function(request,response){
-    switch(request.url){
-        case "/led/on":
-            console.log('Turn on led');
-            GPIO.on();
-            response.end('Turn on led');
-            break;
-        case "/led/off":
-            console.log('Turn off led');
-            GPIO.off();
-            response.end('Turn off led');
-            break;
-        default:
-            console.log('I dont know man');
-            response.end('I dont know man');
-    }
-})
-    .listen(8000,function(){
-    console.log('Server listening on 8000');
-});
-
+function startHttpServer() {
+    http
+        .createServer(function (request, response) {
+            switch (request.url) {
+                case "/led/on":
+                    console.log('Turn on led');
+                    LED.on();
+                    response.end('Turn on led');
+                    break;
+                case "/led/off":
+                    console.log('Turn off led');
+                    LED.off();
+                    response.end('Turn off led');
+                    break;
+                default:
+                    console.log('I dont know man');
+                    response.end('I dont know man');
+            }
+        })
+        .listen(8000, function () {
+            console.log('Server listening on 8000');
+        });
+}
