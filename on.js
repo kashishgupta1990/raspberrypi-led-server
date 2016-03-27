9915b4f8ef97040c969c4e21ed340c79501fcfd0
Shubham Gupta <charmshubham@gmail.com>
2016-03-27 14:38:24 +0530

    sdf

--- on.js
+++ on.js
@@ -1,10 +1,7 @@
-var gpio = require('rpi-gpio');
+var gpio = require("pi-gpio");
 
-gpio.setup(16, gpio.DIR_OUT, write);
-
-function write() {
-    gpio.write(16, true, function(err) {
-        if (err) throw err;
-        console.log('Written to pin');
+gpio.open(16, "output", function(err) {         // Open pin 16 for output
+    gpio.write(16, 1, function() {                  // Set pin 16 high (1)
+        gpio.close(16);                                         // Close pin 16
     });
-}
+});
