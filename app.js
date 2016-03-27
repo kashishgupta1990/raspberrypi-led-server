907f37c0d994bee3ff16fa546915d5dc633efab5
Shubham Gupta <charmshubham@gmail.com>
2016-03-27 15:39:34 +0530

    adde on off + check state

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var gpio = require('pi-gpio');
 var GPIO_PIN = {
   SIXTEEN:16  
 };
+
+var PIN_STATE={
+    ON:1,
+    OFF:0
+};
+
 gpio.open(GPIO_PIN.SIXTEEN, "output", function(err) {		// Open pin 16 for output
     if(err){
         throw err;
@@ -13,22 +19,20 @@ gpio.open(GPIO_PIN.SIXTEEN, "output", function(err) {		// Open pin 16 for output
     startHttpServer();
 });
 
+function closeAllPins(){
+    gpio.close(GPIO_PIN.SIXTEEN);
+}
+
 var LED = {
     on:function(){
-        gpio.read(GPIO_PIN.SIXTEEN, function(err, value) {
-            if(err) throw err;
-            if(value == 0){
-                gpio.write(GPIO_PIN.SIXTEEN, 1, function() {});
-            }
-        });
+        gpio.write(GPIO_PIN.SIXTEEN, PIN_STATE.ON, function() {});
+        
     },
     off:function(){
-        gpio.read(GPIO_PIN.SIXTEEN, function(err, value) {
-            if(err) throw err;
-            if(value == 1){
-                gpio.close(GPIO_PIN.SIXTEEN);
-            }
-        });
+        gpio.write(GPIO_PIN.SIXTEEN, PIN_STATE.OFF, function() {});
+    },
+    state:function(callback){
+        gpio.read(GPIO_PIN.SIXTEEN, callback);
     }
 };
 
@@ -46,6 +50,20 @@ function startHttpServer() {
                     LED.off();
                     response.end('Turn off led');
                     break;
+                case "/led/state":
+                    console.log('Turn off led');
+                    LED.state(function(err,value){
+                       if(err){
+                           response.end('Some error occure');
+                       } else{
+                           if(value==1){
+                               response.end('ON');
+                           }else {
+                               response.end('OFF');
+                           }
+                       }
+                    });
+                    break;
                 default:
                     console.log('I dont know man');
                     response.end('I dont know man');
@@ -53,5 +71,9 @@ function startHttpServer() {
         })
         .listen(8000, function () {
             console.log('Server listening on 8000');
-        });
+        })
+        .on('close', function() {
+            closeAllPins();
+            console.log('Http Serve Closed');
+         });
 }
